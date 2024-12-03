e88a354ff05e0825dd8b0b6af93d503fc0d6b527
AhmedMK <152314340+Ahmedmohamedk@users.noreply.github.com>
2024-12-03 22:04:25 +0200

    jdk to 20

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Human!!!!!");
+        System.out.println("Ahmeddddddddd");
+        // ya allllah
 
     }
 }
