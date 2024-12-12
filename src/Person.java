4045c0153666b7afc8dccc9863a1e761342a46be
Unknown <2023170400@cis.asu.edu.eg>
2024-12-12 18:25:38 +0200

    Finished Register
    Finished Register Feature for both admin and student to be able to check for all registered users and also can check for the current user's exam history
    
    and even create new user and check for existing exams, take it, and see the results

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Person <T>{
+public class Person{
     public int ID;
     public String name;
     public String role;
@@ -20,36 +20,33 @@ public class Person <T>{
         return password;
     }
 
-    public void viewResults()
-    {}
-
     static public List<String> Login(List<Person> people)
     {
         boolean userFound = false;
         Scanner reader = new Scanner(System.in);
         List<String> values = new ArrayList<>();
-        String email;
-        String password;
-        System.out.print("Enter your Email: ");
-        email = reader.nextLine();
-        System.out.print("Enter your password: ");
-        password = reader.nextLine();
 
-        for (Person person : people) {
-            if (email.equals(person.Email) && password.equals(person.getPassword())) {
-                values.add(String.valueOf(person.ID));
-                values.add(String.valueOf(person.name));
-                values.add(String.valueOf(person.Email));
-                values.add(String.valueOf(person.getPassword()));
-                values.add(String.valueOf(person.role));
-                userFound = true;
-                break;
+        do {
+            String email;
+            String password;
+            System.out.print("Enter your Email: ");
+            email = reader.nextLine();
+            System.out.print("Enter your password: ");
+            password = reader.nextLine();
+
+            for (Person person : people) {
+                if (email.equals(person.Email) && password.equals(person.getPassword())) {
+                    values.add(String.valueOf(person.ID));
+                    values.add(String.valueOf(person.name));
+                    values.add(String.valueOf(person.Email));
+                    values.add(String.valueOf(person.getPassword()));
+                    values.add(String.valueOf(person.role));
+                    userFound = true;
+                    break;
+                }
             }
         }
-
-        if (!userFound) {
-            Login(people);
-        }
+        while(!userFound);
 
         return values;
     }
--- src/Student.java
+++ src/Student.java
@@ -3,42 +3,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Student extends Person {
+public class Student <T> extends Person {
 
     Scanner reader = new Scanner(System.in);
-    public static List<Person> newerCredentials = new ArrayList<Person>();
-
+    public static List<Person> newerCredentials = new ArrayList<>();
     Solve_Exam exam;
     public Student(int id,String name, String email, String password, String role) throws IOException {
       super(id,name,email,password, role);
     }
 
 
-    public void viewResults(List<String> student, Result upComingResult) {
-        Result result = upComingResult;
-        result.displayStudentResults();
+    public void viewResults(Result upComingResult) {
+        upComingResult.displayStudentResults();
     }
 
-    public void takeExam(List<String> student, Result result) {
+    public void takeExam(List<String> data, Result result){
 
         if(!AdminMakeQuestions.createdExam.isEmpty())
         {
-
             for (int i = 0; i < AdminMakeQuestions.createdExam.size(); i++)
             {
-                System.out.println("[" + (i) +"]" + AdminMakeQuestions.createdExam.get(i).examTitle);
+                System.out.println("[" + (i) + "]" + AdminMakeQuestions.createdExam.get(i).examTitle);
             }
 
             System.out.print("Enter the number of Exam to take: ");
             int examNumber = reader.nextInt();
-            if(examNumber >= 0 && examNumber < AdminMakeQuestions.createdExam.size() )
+            if(examNumber >= 0 && examNumber < AdminMakeQuestions.createdExam.size())
             {
-                exam = new Solve_Exam();
-                Solve_Exam.SolveExam(examNumber, result);
+                    exam = new Solve_Exam();
+                    Solve_Exam.SolveExam(examNumber, result);
             }
             else
             {
-                takeExam(student, result);
+                System.out.println("\t\txxxxxxxxxxxx Invalid option  xxxxxxxxxxxx\n");
+                takeExam(data, result);
             }
         }
         else
@@ -49,7 +47,6 @@ public class Student extends Person {
 
     static public void register(int LastIdIndex) throws IOException {
         Scanner reader = new Scanner(System.in);
-        int currentIndex = LastIdIndex;
         System.out.println("\t\t==========================================  Welcome to Register Page ==========================================\n\n");
         System.out.print("Name: ");
         String name = reader.nextLine();
@@ -57,8 +54,6 @@ public class Student extends Person {
         String email = reader.nextLine();
         System.out.print("Password: ");
         String password = reader.nextLine();
-        newerCredentials.add(new Student(++currentIndex,name,email,password,"student"));
+        newerCredentials.add(new Student(LastIdIndex,name,email,password,"student"));
     }
-
-
 }
